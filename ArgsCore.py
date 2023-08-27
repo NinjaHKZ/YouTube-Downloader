514b3a1e37f02443a49c5a03ed9f2b34516faba5
Marcos Henrique Albach <ninjahkz0@gmail.com>
2023-08-27 00:02:28 -0300

    download de áudios e arquivo de downloads
    adicionado forma de baixar áudio dos vídeos apenas, correção de alguns bugs e agora as urls podem ser passadas junto com arquivos de texto para baixar tudo.

--- ArgsCore.py
+++ ArgsCore.py
@@ -14,14 +14,26 @@ class ArgEngine:
 
         args.add_argument('-u', '--url', required=True, nargs="+", type=str,
                         help='Define a url, pode ser separada por vírgulas para multiplos downloads, deve ser link ou id.')
-        args.add_argument('-r', '--resolution', default=144, nargs="+", type=str,
+        args.add_argument('-r', '--resolution', default="144", nargs="+", type=str,
                         help='define a qualidade(deve ser inteiro: 144 e não 144p), separar por vírgula para multiplas resoluções.')
-        args.add_argument('-m', '--mediatype', choices=['video', 'audio'], default='video', nargs=1, type=str,
+        args.add_argument('-m', '--mediatype', choices=['video', 'audio'], default='video', type=str,
                         help='define um tipo de mídia desejada, sendo vídeo ou áudio.')
         args.add_argument('-p', '--path', type=str, default='download/',
                         help='Define o caminho onde deseja salvar os items.')
         args.add_argument('-utasks', '--url_per_tasks', type=int, default=3,
                         help='Define a queue de download ao mesmo tempo(1 para 1 url por task, 2 para 2 para cada tasks e etc...).')
 
-        return args.parse_args()
+        
+
+        args = args.parse_args()
+
+        for url in args.url:
+            if url.endswith('.txt') == True:
+                args.url.remove(url)
+
+                with open(url, 'r') as r:
+                    for lines in r.readlines():
+                        args.url.append(lines)
+        
+        return args
 
--- main.py
+++ main.py
@@ -12,41 +12,53 @@ class DownloadManager:
         arguments.url = set(arguments.url)
         
         self.urls = [res.replace(',', '') for res in arguments.url]
-        try:
-            self.resolution = [int(res.replace(',', '')) for res in arguments.resolution]
-        except ValueError:
-            print('resolutions must be int.')
-            exit()
-
-        self.format = arguments.mediatype
+        
+        self.media_type = arguments.mediatype
         self.path = arguments.path
         self.simultaneous_task = arguments.url_per_tasks
+        
+
+        if self.media_type == "audio":
+            self.itag_identifier = {
+                140: (None, "mp3")
+                }
+
+        else:
+            try:
+                self.resolution = [int(res.replace(',', '')) for res in arguments.resolution]
+                        
+                self.itag_identifier = {
+                    17: (144, "mp4"),
+                    18: (360, "mp4"),
+                    22: (720, "mp4"),
+                    37: (1080, "mp4"),
+                    38: (3072, "mp4"),
+                    160: (144, "mp4")
+                    }
+
+                sup_resolutions = [res[0] for res in list(self.itag_identifier.values())]
+                remove_resolutions = []
+                
+                for res in enumerate(self.resolution):
+                    if res[1] not in sup_resolutions:
+                        remove_resolutions.append(res[1])
+                        
+                
+                if len(self.resolution) == len(remove_resolutions):
+                    self.resolution = [360]
+
+                elif len(remove_resolutions) != 0:
+                    for index in remove_resolutions:
+                        self.resolution.remove(index)
+            
+            except ValueError:
+                print('resolutions must be int.')
+                exit()
+
 
         self.tasks_queue = [] 
         self.content_filtered = {}
 
-        self.itag_identifier = {
-            18: (360, "mp4"),
-            22: (720, "mp4"),
-            37: (1080, "mp4"),
-            38: (3072, "mp4"),
-            160: (144, "mp4")
-            }
-
-        sup_resolutions = [res[0] for res in list(self.itag_identifier.values())]
-        remove_resolutions = []
-        
-        for res in enumerate(self.resolution):
-            if res[1] not in sup_resolutions:
-                remove_resolutions.append(res[1])
-                
-        
-        if len(self.resolution) == len(remove_resolutions):
-            self.resolution = [360]
-
-        elif len(remove_resolutions) != 0:
-            for index in remove_resolutions:
-                self.resolution.remove(index)
         
     def start(self):
         loop = asyncio.get_event_loop()
@@ -80,28 +92,41 @@ class DownloadManager:
 
     async def _get_stream_data(self, url):
         request_results = YouTube(url)
-        self.content_filtered.update({request_results.title: {}})
-        
-        for data in request_results.streaming_data['formats']:
-            self.content_filtered[request_results.title].update({data["itag"]: {}})
-            
-            await asyncio.sleep(0.015)
-            dict_filter_type = {
-                "name": request_results.title,
-                "creator_name": request_results.author,
-                "itag": data['itag'],
-                "mimeType": data['mimeType'],
-                "media_url": data['url'],
-                "vide_quality": data["quality"],
-                "audio_quality": data['audioQuality'],
-                "midia_fps": data['fps'],
-                "height_resolution": data["qualityLabel"]
+        author = request_results.author
+        title = request_results.title
+        request_results = request_results.streaming_data['formats'] if self.media_type == "video" else request_results.streams.get_audio_only()
 
+        self.content_filtered.update({title: {}})
+        
+        if self.media_type == "video":
+            for data in request_results:
+
+                self.content_filtered[title].update({data["itag"]: {}}) 
+
+                dict_filter_type = {
+                    "name": title,
+                    "creator_name": author,
+                    "itag": data['itag'],
+                    "mimeType": data['mimeType'],
+                    "media_url": data['url'],
+                    "height_resolution": data['height']
+                }
+                await asyncio.sleep(0.015)
+                self.content_filtered[title][dict_filter_type['itag']].update(dict_filter_type)
+            
+        else:
+            self.content_filtered[title].update({request_results.itag: {}})
+            
+            dict_filter_type = {
+                "name": title,
+                "creator_name": author,
+                "itag": request_results.itag,
+                "mimeType": request_results.mime_type,
+                "media_url": request_results.url
             }
             await asyncio.sleep(0.015)
-
             self.content_filtered[request_results.title][dict_filter_type['itag']].update(dict_filter_type)
-
+    
     async def _start_download(self, content):
         content_for_downlaod = []
         
@@ -109,17 +134,24 @@ class DownloadManager:
             if content_dict != None:
                 for itags in content_dict[1].items():
                     await asyncio.sleep(0.015)
-                    
+                    print(content_dict)
                     if itags[0] in list(self.itag_identifier.keys()):
+                        if self.media_type == "video":
                             if self.itag_identifier[itags[0]][0] in self.resolution:
+
                                 title = "{}{} {} {}.{}".format(self.path, itags[1]["height_resolution"], itags[1]['name'], itags[1]['creator_name'], self.itag_identifier[itags[0]][1])
-                                content_for_downlaod.append((itags[1].get('media_url'), title.replace(' ', '+'), self.path))
+                                content_for_downlaod.append((itags[1].get('media_url'), title.replace(' ', '+').replace('|', ''), self.path))
+                            else:
+                                continue
+                        
+                        else:
+                            title = "{}{} {}.{}".format(self.path, itags[1]['name'], itags[1]['creator_name'], self.itag_identifier[itags[0]][1])                            
+                            content_for_downlaod.append((itags[1].get('media_url'), title.replace(' ', '+').replace('|', ''), self.path))
 
                     else:
                         pass
-                       # print("processo não suportado", itags[1], itags[0])
-            else:
-                pass
+                       #print("processo não suportado", itags[1], itags[0])
+                    
         await DownloadEngine(content_for_downlaod).start()
         
 
@@ -134,7 +166,6 @@ class DownloadManager:
         else:
             return {'error': 'Use only LINK or ID from video.'}, 0, "\ninvalid Option: "+url
             
-
 
 
 if __name__ == "__main__":
